23764324988b2c4a8f66ae34ea986acd7c8dc550
Abhishek Gupta <hi@itsabhishek.me>
2019-11-23 15:18:55 +0530

    bleh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ COMMAND14="cd ~/frontends/amoeba-frontpages/online.codingblocks.com && \
 	git reset --hard origin/master && \
 	git pull && \
     ulimit -S -n 65000 && \
+    nvm use && \
   yarn && yarn build && \
+  nvm use 8 && \
   pm2 restart amoeba-frontpages --update-env
 	"
 
