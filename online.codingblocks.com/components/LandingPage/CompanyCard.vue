c43d4e888c293905b59c148af9fac25c1a0542c8
frankenstein32 <saifirashid32@gmail.com>
2019-12-20 20:16:19 +0530

    Partial Landing-page-comp  added

---
+++ online.codingblocks.com/components/LandingPage/CompanyCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-4 mb-5" style="background: linear-gradient(90deg, #00205B 0%, #84358E 100%)">
+    <div class="container p-4">
+      <div class="row">
+        <div class="ml-3 mr-5 col-12 col-md-3 t-align-c t-align-md-l my-auto">
+          <div class="font-md" style="color:white">Land your dream job</div>
+          <div class="font-sm" style="color: white;">Our alumi work at</div>
+        </div>
+        <div class="col-12 col-md-7 mr-1">
+          <div style="color: white;">
+            <img src="http://via.placeholder.com/900x60?text=CompanyStrip" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ online.codingblocks.com/components/LandingPage/InfoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container a-ocb">
+    <div class="row ml-3 mt-4">
+      <div class="col-12 col-md-5 p-4">
+        <div class="gradient-text-orange font-lg p-1">Uplift your career with us!</div>
+        <div class="font-md mb-4 p-1">We will help you build skills & land your dream job</div>
+        <div class="grey mb-3 pt-3">Let's get to know your preferences first</div>
+        <div class="border-card">
+          <div class="mb-3">
+            I am a
+            <button class="button-solid button-orange ml-2">Student</button>
+            <button class="button-dashed button-orange ml-2">Professional</button>
+          </div>
+          <div class="mb-3 pt-3">
+            I want to become a
+            <select>
+              <option value="xxx">Web Development</option>
+            </select>
+          </div>
+          <button class="button-solid button-orange">Continue</button>
+        </div>
+      </div>
+      <div class="offset-0 col-12 offset-md-1 col-md-6 d-none d-md-block">
+        <img src="https://minio.codingblocks.com/amoeba/hellointern-banner.svg" alt srcset />
+      </div>
+    </div>
+  </div>
+</template>
+
