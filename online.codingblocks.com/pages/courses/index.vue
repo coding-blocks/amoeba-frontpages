634fa3aaa0fdc718e8ceb11b658f72cc7ebb9a01
Abhishek Gupta <hi@itsabhishek.me>
2020-01-06 18:22:48 +0530

    add seach to /courses

--- online.codingblocks.com/pages/courses/index.vue
+++ online.codingblocks.com/pages/courses/index.vue
@@ -7,7 +7,7 @@
             </div>
             
             <div class="input-search d-flex col-lg-7 col-md-6 col-10 bg-light-grey">
-                <input class="bg-light-grey" placeholder="What do you want to learn?" type="text">
+                <input class="bg-light-grey" placeholder="What do you want to learn?" type="text" v-model="searchQuery">
             </div>
         </div>
         <div class="row a-ocb">
@@ -31,10 +31,7 @@ export default {
         exclude: `ratings,instructors.*`,
         include: `instructors,runs`,
         filter: {
-          unlisted: false,
-          domains: {
-            $contains: ['hellointern']
-          }
+          unlisted: false
         },
         page: {
           limit: 100
@@ -46,7 +43,35 @@ export default {
   },
   data () {
     return {
-      courses: [] 
+      courses: [],
+      searchQuery: null
+    }
+  },
+  tasks (t, { timeout }) {
+    return {
+      search: t(async function () {
+        // await timeout(1000)
+
+        const res = await this.$axios.get('/courses', {
+          params: {
+            exclude: `ratings,instructors.*`,
+            include: `instructors,runs`,
+            filter: {
+              title: {
+                $iLike: `%${this.searchQuery}%`
+              },
+              unlisted: false
+            },
+            page: {
+              limit: 100
+            }
+          }
+        })
+
+        this.courses = this.$jsonApiStore.sync(res.data)
+      })
+      .flow('restart', { delay: 400 })
+      .runWith('searchQuery')
     }
   }
 }
