fb0c78aa4fd61ea4e7a18d1d8e4a2a6766931aad
Abhishek Gupta <hi@itsabhishek.me>
2020-01-08 19:57:03 +0530

    sidebar fix

--- online.codingblocks.com/components/TheSideBar.vue
+++ online.codingblocks.com/components/TheSideBar.vue
@@ -7,31 +7,31 @@
       <div class="col-2 col-md-12">
         <a href="/app/dashboard" class="nav-element py-4 px-2">
           <FaIcon icon="chart-pie" class="fas fa-chart-pie font-xl"></FaIcon>
-          <h6 class="pt-2 d-none d-md-block">Dashboard</h6>
+          <h6 class="pt-2 d-none d-md-block font-sm">Dashboard</h6>
         </a>
       </div>
       <div class="col-2 col-md-12">
         <a href="#buy" class="nav-element py-4 px-2 active">
           <FaIcon icon="search" class="fas fa-search font-xl"></FaIcon>
-          <h6 class="pt-2 d-none d-md-block">Buy Courses</h6>
+          <h6 class="pt-2 d-none d-md-block font-sm">Buy Courses</h6>
         </a>
       </div>
       <div class="col-2 col-md-12">
         <a href="/app/tracks" class="nav-element py-4 px-2">
           <FaIcon icon="graduation-cap" class="fas fa-graduation-cap font-xl"></FaIcon>
-          <h6 class="pt-2 d-none d-md-block">Tracks</h6>
+          <h6 class="pt-2 d-none d-md-block font-sm">Tracks</h6>
         </a>
       </div>
       <div class="col-2 col-md-12">
         <a href="/app/classroom" class="nav-element py-4 px-2">
           <FaIcon icon="video" class="fas fa-video font-xl"></FaIcon>
-          <h6 class="pt-2 d-none d-md-block">My Courses</h6>
+          <h6 class="pt-2 d-none d-md-block font-sm">My Courses</h6>
         </a>
       </div>
       <div class="col-2 col-md-12">
         <a href="/app/jobs" class="nav-element py-4 px-2">
           <FaIcon icon="suitcase" class="fas fa-suitcase font-xl"></FaIcon>
-          <h6 class="pt-2 d-none d-md-block">Hiring Blocks</h6>
+          <h6 class="pt-2 d-none d-md-block font-sm">Hiring Blocks</h6>
         </a>
       </div>
     </div>
