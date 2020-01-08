c73e75423f975cd1b48327eeef83f6a088d64eda
Abhishek Gupta <hi@itsabhishek.me>
2020-01-08 16:02:07 +0530

    add logout

---
+++ online.codingblocks.com/pages/logout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container t-align-c mt-5">
+    Logging you out....
+  </div>
+</template>
+
+<style scoped>
+div {
+  height: 50vh;
+}
+</style>
+
+<script>
+export default {
+  mounted () {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start()
+      
+      window.localStorage.clear()
+      this.$cookies.remove('auth-jwt')      
+      setTimeout(() => {
+        this.$nuxt.$loading.finish()
+        window.location.href = "/"
+      }, 0)
+    })
+  }
+}
+</script>
