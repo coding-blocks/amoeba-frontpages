06b20d58d4efd44a101d539b5ce72ec5ff14dd7c
Abhishek Gupta <hi@itsabhishek.me>
2020-01-26 16:16:22 +0530

    add favicons and title

--- online.codingblocks.com/nuxt.config.js
+++ online.codingblocks.com/nuxt.config.js
@@ -9,7 +9,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Coding Blocks Online',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -19,7 +19,23 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{
+      rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico',
+      rel: 'apple-touch-icon', sizes: '57x57', href: '/favicons/apple-icon-57x57.png',
+      rel: 'apple-touch-icon', sizes: '60x60', href: '/favicons/apple-icon-60x60.png',
+      rel: 'apple-touch-icon', sizes: '72x72', href: '/favicons/apple-icon-72x72.png',
+      rel: 'apple-touch-icon', sizes: '76x76', href: '/favicons/apple-icon-76x76.png',
+      rel: 'apple-touch-icon', sizes: '114x114', href: '/favicons/apple-icon-114x114.png',
+      rel: 'apple-touch-icon', sizes: '120x120', href: '/favicons/apple-icon-120x120.png',
+      rel: 'apple-touch-icon', sizes: '144x144', href: '/favicons/apple-icon-144x144.png',
+      rel: 'apple-touch-icon', sizes: '152x152', href: '/favicons/apple-icon-152x152.png',
+      rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-icon-180x180.png',
+      rel: 'icon', type: 'image/png', sizes: '192x192',  hre: '/favicons/android-icon-192x192.png',
+      rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png',
+      rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicons/favicon-96x96.png',
+      rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png',
+      rel: 'manifest', href: '/favicons/manifest.json' 
+    }]
   },
   /*
    ** Customize the progress-bar color
