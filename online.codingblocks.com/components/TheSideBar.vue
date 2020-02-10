ec400f05d248c17430d5620ce4956382b7bd91df
Jatin Katyal <jatin.katyal13@gmail.com>
2020-02-10 14:22:22 +0530

    add valentine's day link

--- online.codingblocks.com/components/TheSideBar.vue
+++ online.codingblocks.com/components/TheSideBar.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="c-sidenav bottom-nav col-md-1 col-12">
-    <div class="logo pb-5 pt-4 d-md-block d-none">
+    <div class="logo pb-4 pt-4 d-md-block d-none">
       <img src="https://minio.codingblocks.com/amoeba/OnlineLogo2020.svg" alt="Logo" class="s-50x50" />
     </div>
     <div class="row all-center">
+      <div class="col-2 col-md-12">
+        <a href="/app/valentines" class="nav-element py-4 px-2">
+          <img src="https://minio.codingblocks.com/amoeba/love.svg" alt="">
+          <h6 class="pt-2 d-none d-md-block font-sm gradient-text-orange">Box of Love</h6>
+        </a>
+      </div>
       <div class="col-2 col-md-12">
         <a href="/app/dashboard" class="nav-element py-4 px-2">
           <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/dashboard_grey.svg"/>
