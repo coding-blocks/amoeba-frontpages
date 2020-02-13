29a7888e4eacb70b297cb020bbad1536fd21ec30
Abhishek Gupta <hi@itsabhishek.me>
2020-02-13 16:48:05 +0530

    add course language

--- online.codingblocks.com/components/AboutCourse/IntroductionCard.vue
+++ online.codingblocks.com/components/AboutCourse/IntroductionCard.vue
@@ -56,7 +56,7 @@
           <div class="col-6 col-md-3">
             <div class="info-item">
             <div>Course Language</div>
-            <div>Hindi, English</div>
+            <div> {{course.language || 'English and Hindi'}} </div>
             </div>
           </div>
           <!-- <div class="col-6 col-md-3">
