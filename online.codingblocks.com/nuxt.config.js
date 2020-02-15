947903b888b85a262c6c5d2f92d913138c878ba0
Abhishek Gupta <hi@itsabhishek.me>
2020-02-15 19:25:28 +0530

    add jivo chat

--- online.codingblocks.com/nuxt.config.js
+++ online.codingblocks.com/nuxt.config.js
@@ -39,7 +39,8 @@ export default {
     }],
     script: [
       { src: 'https://hack.codingblocks.com/index.js' },
-      { src: 'https://unpkg.com/@coding-blocks/web-components@0.0.4/dist/index.js'}
+      { src: 'https://unpkg.com/@coding-blocks/web-components@0.0.4/dist/index.js' },
+      { src: 'https://code.jivosite.com/widget.js', 'data-jv-id': 'ASYGLZQ6UC' }
     ]
   },
   /*
