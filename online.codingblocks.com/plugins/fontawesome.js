0d3ad44b677301659cc49467e4f007c55a70b05a
Arnav Gupta <dev@championswimmer.in>
2020-03-23 18:55:00 +0000

    fix typo and reformat fontawesome

--- online.codingblocks.com/plugins/fontawesome.js
+++ online.codingblocks.com/plugins/fontawesome.js
@@ -1,14 +1,43 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar, faAngleUp, faAngleDown, faLock, faPlayCircle, faChartPie, faSearch, faGraduationCap, faVideo, faSuitcase, faSlash, faBell, faShoppingCart,faFileExcel,faFileCode,faFileWord, faExclamationTriangle, faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import {
+  faStar,
+  faAngleUp,
+  faAngleDown,
+  faLock,
+  faPlayCircle,
+  faChartPie,
+  faSearch,
+  faGraduationCap,
+  faVideo,
+  faSuitcase,
+  faBell,
+  faShoppingCart,
+  faFileExcel,
+  faFileCode,
+  faFileWord,
+  faExclamationTriangle,
+  faLightbulb
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-[
-  faStar, faAngleUp, faAngleDown, faLock, faPlayCircle,
-  faChartPie, faSearch, faGraduationCap, faVideo, faSuitcase, faLihtbulb
-  faBell, faShoppingCart,faFileExcel,faFileCode,faFileWord, faExclamationTriangle
-]
-  .forEach(icon => library.add(icon))
+;[
+  faStar,
+  faAngleUp,
+  faAngleDown,
+  faLock,
+  faPlayCircle,
+  faChartPie,
+  faSearch,
+  faGraduationCap,
+  faVideo,
+  faSuitcase,
+  faLightbulb,
+  faBell,
+  faShoppingCart,
+  faFileExcel,
+  faFileCode,
+  faFileWord,
+  faExclamationTriangle
+].forEach((icon) => library.add(icon))
 
 Vue.component('FaIcon', FontAwesomeIcon)
-
