47b2954c5b7caeb6d7e858d9876ba14529e5bb27
1arp <dekhijaegi@gmail.com>
2020-04-16 20:37:07 +0530

    handle 404

---
+++ online.codingblocks.com/components/Error/404.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="all-center h-100">
+    <div class="w-60 t-align-c">
+      <div class="extra-bold font-superlarge" style="font-size: 10rem">404</div>
+      <div class="light subheading font-xl">
+        Page not found.
+        <a class="orange" href="/courses">
+          Click here to explore our courses
+        </a>
+      </div>
+
+      <img
+        src="https://minio.codingblocks.com/amoeba/404-image-min.png"
+        alt="404"
+        class="mt-1"
+      />
+    </div>
+  </div>
+</template>
---
+++ online.codingblocks.com/layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="errorComponent" />
+</template>
+<script>    
+export default {
+    props : {
+        error : {
+            type : Object,
+            required: true,
+        }
+    },
+    layout: 'landing-page',
+    computed : {
+        errorComponent: function(){
+            return () => import(`../components/Error/${this.error.statusCode}`)
+        }
+
+    }
+}
+</script>
