c960f4a031c4c4f7a23304f956e42b47cccbe66d
1arp <dekhijaegi@gmail.com>
2020-05-06 20:31:41 +0530

    implement accordian into featured tags

--- online.codingblocks.com/components/Base/FeaturedTagExplore.vue
+++ online.codingblocks.com/components/Base/FeaturedTagExplore.vue
@@ -1,17 +1,36 @@
-<template>
-  <div v-show="featuredTag.tag.courses.length">
-    <div class="my-3 font-normal font-md">{{featuredTag.name}}</div>
-    <div class="w-100 h-100 row">
-        <CourseCard
-          :course="course"
-          v-for="course in featuredTag.tag.courses"
-          :key="course.id"
-        />
-    </div>
-  </div>
+`<template>
+  <VAaccordian>
+    <template v-slot:head="{ onToggle, expanded }">
+      <div v-show="featuredTag.tag.courses.length">
+        <div class="row mb-4">
+        <h4 class="font-lg col-10 capitalise">{{featuredTag.name}} ({{featuredTag.tag.courses.length}})</h4>
+        <div class="my-auto font-normal font-md orange underline col-2" @click="onToggle" v-show="expandedCourses.length">{{expanded? 'Hide':'View All'}}</div>
+        </div>
+        <div class="w-100 h-100 row">
+            <CourseCard
+              :course="course"
+              v-for="course in collapsedCourses"
+              :key="course.id"
+            />
+        </div>
+      </div>
+    </template>
+    <template v-slot:content >
+      <div class="w-100 h-100 row">
+            <CourseCard
+              :course="course"
+              v-for="course in expandedCourses"
+              :key="course.id"
+            />
+        </div>
+    </template>
+  </VAaccordian>
 </template>
+
+
 <script>
   import CourseCard from './CourseCard'
+  import VAaccordian from './VAccordion'
   
   export default {
     name: 'FeaturedTagExplore',
@@ -21,11 +40,29 @@
       }
     },
     components: {
-      CourseCard
+      CourseCard,
+      VAaccordian
     },
+    computed:{
+      sortedCourses(){
+        return this.featuredTag.tag.courses.sort((a,b)=> a.runs[0].price - b.runs[0].price)
+      },
+      collapsedCourses(){
+        return this.sortedCourses.slice(0,3)
+      },
+      expandedCourses(){
+        return this.sortedCourses.slice(3)
+
+      }
+    }
 
   }
 </script>
 <style>
-
-</style>
+.underline{
+  text-decoration: underline;
+}
+.capitalise{
+  text-transform: capitalize;
+}
+</style>`
