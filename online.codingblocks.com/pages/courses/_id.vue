55a8d9f70924f6a2544d653f64a7569df5f77903
Ashvary <ashvaryvirmani@gmail.com>
2020-05-06 17:02:49 +0530

    resolving conflicts

--- online.codingblocks.com/pages/courses/_id.vue
+++ online.codingblocks.com/pages/courses/_id.vue
@@ -83,7 +83,7 @@ import LeadGenerationCard from '~/components/AboutCourse/LeadGenerationCard.vue'
 import StudentsExperience from '~/components/LandingPage/StudentsExperience.vue'
 import SuggestedTrackCard from '~/components/AboutCourse/SuggestedTrackCard.vue'
 import AlternateTrackCard from '~/components/AboutCourse/AlternateTrackCard.vue'
-
+import ChooseRunTier from '~/components/AboutCourse/ChooseRunTier/Index.vue'
 
 import sidebarLayoutMixin from '~/mixins/sidebarForLoggedInUser'
 
@@ -141,7 +141,8 @@ export default {
     StudentsExperience,
     VAsync,
     SuggestedTrackCard,
-    AlternateTrackCard
+    AlternateTrackCard,
+    ChooseRunTier
   },
   computed: {
     projectIds() {
