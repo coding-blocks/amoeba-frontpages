af6dc21c98021b34b49e00fe17c89c0f867467c9
Abhishek Gupta <hi@itsabhishek.me>
2020-05-09 19:34:42 +0530

    *goodies not available for LITE batches

--- online.codingblocks.com/components/AboutCourse/WildcraftCard.vue
+++ online.codingblocks.com/components/AboutCourse/WildcraftCard.vue
@@ -5,9 +5,20 @@
         <img src="https://minio.codingblocks.com/amoeba/wildcraft-fire-free.png" alt="fire">
       </div>
       <img src="https://minio.codingblocks.com/amoeba/wildcraft-logo.png" alt="wildcraft" class="my-4">
-      <div class="font-mds font-normal">
-        Get a <span class="bold gradient-text-orange">Wildcraft Backpack</span> and lots of goodies with this course!
+      <div class="font-mds font-normal mb-2">
+        Get a <span class="bold gradient-text-orange">Wildcraft Backpack</span> and lots of goodies with this course!*
+      </div>
+      <div class="tnc p-3 font-xs">
+        *Not applicable for LITE batches. 
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.tnc {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+</style>
