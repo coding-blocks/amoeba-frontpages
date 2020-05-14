fa1fbb4939948769a5f8c83d824ce372608d8986
BipinKalra <bipinkalra@gmail.com>
2020-05-14 06:22:06 +0530

    detailed review modal changes

--- online.codingblocks.com/components/AboutCourse/DetailedReviews.vue
+++ online.codingblocks.com/components/AboutCourse/DetailedReviews.vue
@@ -1,40 +1,70 @@
 <template>
   <div class="mymodal">
-    <header class="modal-header" id="modalTitle">
-      <div class="mb-4 mt-4 ml-3 mr-3">
-        <img class="float-left mr-3 ml-2" src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/reviews_tag.svg" />
-        <div class="float-left">
-          <span class="font-md">{{courseName}}</span><br>
-          <span class="font-lg extra-bold">Customer Reviews</span>
-        </div>
-        <div class="float-right">
-          <div class="bold font-sm float-right mt-2">{{ ratingStats.count }} Ratings</div>
-          <div class="float-right mt-2">
-            <RatingStars pos-rating-class="mr-3" neg-rating-class="d-none" :value="ratingStats.rating" />
-          </div>
-          <div class="bold font-xlg float-right mr-2">{{ ratingStats.rating }} / 5.0</div>
-        </div>
-      </div>
-    </header>
-    <section class="mt-4 ml-5 mr-3" id="modalDescription" v-infinite-scroll="() => this.loadMore.run()" infinite-scroll-disabled="disabledInfiniteScroll" infinite-scroll-distance="10">
-      <slot class="ml-3 mt-2" name="body">
-        <div class="row no-gutters align-items-center mb-3" v-for="review in modalReviews" :key="review.id">
-          <div><img class="s-50x50 br-50 bg-grey mr-4 user_image" :src="review.user.photo" /></div>
-          <div class="col-10">
-            <div class="card-md bold capital">
-              <h4>{{ review.user.firstname }} {{ review.user.lastname }}</h4>
-            </div>
-            <div class="mt-1">
-              <RatingStars pos-rating-class="mr-2" neg-rating-class="d-none" :value="review.value" />
+    <div class="border-card p-md-5 p-4">
+      <header class="modal-header border-none" id="modalTitle">
+        <div
+          class="row no-gutters justify-content-between align-items-center mb-5 pb-4"
+        >
+          <div class="col-lg-6 col-12">
+            <div class="row no-gutters align-items-center">
+              <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/reviews_tag.svg" />
+              <div class="flex-1 pl-4">
+                <div style="letter-spacing: 0.1rem;">{{ courseName }}</div>
+                <h2 class="bold">Customer Reviews</h2>
+              </div>
             </div>
           </div>
-          <div class="col-12">
-            <p class="font-sm mb-1 mt-2">{{ review.heading }}</p>
+          <div class="col-lg-6 col-12 mt-lg-0 mt-3">
+            <div
+              class="row no-gutters align-items-center justify-content-lg-end justify-content-center"
+            >
+              <div class="font-mdxl extra-bold">{{ ratingStats.rating }}/5.0</div>
+              <div class="d-flex align-items-center ml-4" style="width: fit-content;">
+                <RatingStars
+                  pos-rating-class="mr-2"
+                  neg-rating-class="mr-2"
+                  :value="ratingStats.rating"
+                />
+                <span class="font-sm bold ml-3">{{ ratingStats.count }}&nbsp;ratings</span>
+              </div>
+            </div>
           </div>
-          <div class="divider-h my-2"></div>
         </div>
-      </slot>
-    </section>
+      </header>
+      <section
+        id="modalDescription"
+        v-infinite-scroll="() => this.loadMore.run()"
+        infinite-scroll-disabled="disabledInfiniteScroll"
+        infinite-scroll-distance="10"
+      >
+        <slot name="body">
+          <div v-for="review in modalReviews" :key="review.id">
+            <div class="row no-gutters justify-content-between align-items-center">
+              <div class="col-sm-6 col-12">
+                <div class="row no-gutters align-items-center">
+                  <img class="round s-70x70 mr-4 bg-gradient-orange" :src="review.user.photo" />
+                  <div class="flex-1">
+                    <h5 class="bold">{{ review.user.firstname }} {{ review.user.lastname }}</h5>
+                    <div class="mt-1">
+                      <RatingStars
+                        pos-rating-class="mr-2"
+                        neg-rating-class="mr-2"
+                        :value="review.value"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-sm-0 mt-2">
+                <img src="/images/company.png" alt />
+              </div>
+            </div>
+            <div class="mt-4">{{ review.heading }}</div>
+            <div class="divider-h my-4"></div>
+          </div>
+        </slot>
+      </section>
+    </div>
   </div>
 </template>
 <script>
@@ -81,52 +111,39 @@ export default {
       return this.loadMore.isActive
     },
     disabledInfiniteScroll() {
-      return !this.modalReviews.length || this.isSearching || this.infiniteScrollDisabled
+      return (
+        !this.modalReviews.length ||
+        this.isSearching ||
+        this.infiniteScrollDisabled
+      )
     }
   },
   tasks(t, { timeout }) {
     return {
       loadMore: t(function*() {
-        debugger;
+        debugger
         this.$nuxt.$loading.start()
         this.infiniteScrollDisabled = true
         const res = yield this.$axios.get(
-          `ratings/course/${this.curCourseId}`, { params: { page: { limit: this.limit, offset: this.offset } } })
+          `ratings/course/${this.curCourseId}`,
+          { params: { page: { limit: this.limit, offset: this.offset } } }
+        )
         this.offset += 5
 
         const newreviews = yield this.$jsonApiStore.sync(res.data)
         if (newreviews.length) {
           this.modalReviews = [...this.modalReviews, ...newreviews]
           this.infiniteScrollDisabled = false
-        } else
-          this.infiniteScrollDisabled = true // no new courses to load
+        } else this.infiniteScrollDisabled = true // no new courses to load
         this.$nuxt.$loading.finish()
       })
     }
   }
 }
-
 </script>
 <style>
-.float-right {
-  float: right;
-}
-
-.float-left {
-  float: left;
-}
-
 .mymodal {
-  height: 800px;
+  height: 80vh;
   overflow: auto;
-}
-
-.user_image {
-  height: 62px;
-  width: 62px;
-}
-
-.font-xlg {
-  font-size: 35px;
 }
 </style>
