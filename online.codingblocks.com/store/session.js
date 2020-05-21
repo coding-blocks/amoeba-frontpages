8baedc42f581ec9e4697de363c131f62f3d8855d
Prajawal Kumar Pandey <prajwalkp@hotmail.com>
2020-05-22 00:05:02 +0530

    default profile image added

--- online.codingblocks.com/store/session.js
+++ online.codingblocks.com/store/session.js
@@ -11,6 +11,9 @@ export const mutations = {
       state.user = user
       state.isAuthenticated = true
     }
+    if(state.user.photo){
+    	state.user.photo="./user-default-pic.png"
+    }
   },
   setToken(state, token) {
     state.token = token
