c72d4fea2fc419226d1d23f2aaa767803e5856ca
Abhishek Gupta <hi@itsabhishek.me>
2020-06-05 23:30:38 +0530

    derp

--- online.codingblocks.com/components/AboutCourse/ChooseRunTier/RunRow.vue
+++ online.codingblocks.com/components/AboutCourse/ChooseRunTier/RunRow.vue
@@ -84,6 +84,7 @@ export default {
       return Math.round((end - start) / (60 * 60 * 24 * 30))
     },
     dukaanPublicUrl() {
+      console.log(config, config[process.env.NODE_ENV])
       return config[process.env.NODE_ENV].dukaan.url
     },
     ...mapState(['session']),
