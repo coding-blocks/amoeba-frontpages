3868bb16bf74a05bd301516d05bc247b024023cc
sanjeetboora <sanjeet.boora97@gmail.com>
2020-06-19 15:28:29 +0530

    Learning Track section design changed

--- online.codingblocks.com/components/LandingPage/LearningTrackCard.vue
+++ online.codingblocks.com/components/LandingPage/LearningTrackCard.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="mb-5">
-    <div class="container">
-      <div class="border-card p-5 bg-gradient-black-lr" style="position: relative;">
-        <div class="d-flex align-items-center mb-5">
-          <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/track_icon.svg" alt loading="lazy" />
-          <h2
-            class="gradient-text-orange bold font-lg ml-3 flex-1"
-          >Introducing: Learning tracks</h2>
-          <img src="https://minio.codingblocks.com/amoeba/new-sticker-3.svg" class="s-70x70 float-right" loading="lazy" />
+  <div class="mb-4 p-5 bg-light-grey learningTrackCards">
+    <div class="container learningTrack">
+      <div class="row justify-content-between mx-0" style="position: relative;">
+        <div class="d-flex flex-1 justify-content-between mb-5 ">
+          <div class="flex-col align-items-start">
+            <h4 class="global-font-heading-color normal font-xl">Introducing</h4>
+            <h2 class="global-font-heading-color extra-bold font-mdxl">Learning Tracks</h2>
+          </div>
+          <button class="button-orange button-dashed d-flex align-items-center help-button">
+            <img src="/img/chat.svg" alt="chat" loading="lazy" />
+            <span class="font-md ml-2">Need Help?</span>
+          </button>
         </div>
 
-        <div class="white normal font-sm">
-          Our expert instructors believe that systematically designed paths can help you to master your skills & achieve your career goals. We've got your back! Learning Tracks are carefully crafted with a step by step guide that saves you the effort to find out which course fits you well, and in what sequence to maximize your learning experience.
+        <div class="dark-grey normal font-md line-height-29">
+          Our expert instructors believe that systematically designed paths can help you to master your skills &amp; achieve your career goals. We've got your back! <span class="bold">Learning Tracks </span> are carefully crafted with a step by step guide that saves you the effort to find out which course fits you well, and in what sequence to maximize your learning experience.
           <br />
-          <br />Happy learning!
+          <span class="bold">Happy learning!</span>
         </div>
 
-        <TrackCards class="mt-5" />
+        <TrackCards />
       </div>
     </div>
   </div>
@@ -29,3 +32,31 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.learningTrack::before {
+  content: "";
+  background: #333333;
+  position: absolute;
+  left: 0rem;
+  height: 70px;
+  width: 10px;
+}
+.learningTrackCards::before {
+  background: url(/img/tagBackground.svg) no-repeat;
+  content: url(/img/newTag.svg);
+  position: relative;
+  top: -3.2rem;
+  left: calc(100% - 14rem);
+  padding: 9px 40px;
+  background-position-x: calc(60%);
+}
+.line-height-29 {
+  line-height: 29px;
+}
+.help-button {
+  height: 50px;
+  border-radius: 25px;
+  padding: 0px 25px;
+}
+</style>
---
+++ online.codingblocks.com/components/LandingPage/TrackCarouselCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="my-5 mx-3 trackCarouselCard">
+    <div class="border-card track-card p-0">
+      <div class="row no-gutters p-23 track-card__head align-items-start" :style="`background-image: url('${track.background}')`" >
+        <img :src="track.logo" :alt="track.slug" class="s-70x70 track-card__head__logo float-right" loading="lazy">
+        <div class="flex-col align-items-start w-100">
+          <h4 class="extra-bold font-md mb-1 w-100">{{ track.name }}</h4>
+          <span class="semi-bold font-xs">{{ track.courses.length }}Courses</span>
+        </div>
+      </div>
+      <div class="justify-content-end align-items-center p-15">
+        <p>
+          {{ track.description }}
+        </p>
+        <a :href="`/app/tracks/${track.slug}`" class="align-items-center"><span class="font-md bold orange mr-2">Begin</span><img src="/img/buttonArrow.svg" alt="chat" class="h-1 pt-1" loading="lazy"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrackCarouselCard',
+  props: ["track"]
+}
+</script>
+
+<style scoped>
+.track-card__head__logo {
+  bottom: -25px;
+  right: 1.5rem;
+  left: auto;
+}
+.track-card__head {
+  height: 110px;
+}
+.track-card__head .flex-col {
+  justify-content: flex-end;
+}
+.h-1 {
+  height: 1rem;
+}
+.p-23 {
+  padding: 2.3rem;
+}
+.p-15{
+  padding: 1.5rem 2.3rem;
+}
+</style>
