1ed4ea7995908dbbb5c76d9b37985083b730d2c3
Abhishek Gupta <hi@itsabhishek.me>
2020-07-24 03:42:54 +0530

    fetch available courses

--- online.codingblocks.com/pages/flash-sale/index.vue
+++ online.codingblocks.com/pages/flash-sale/index.vue
@@ -24,6 +24,41 @@ export default {
     HeroBanner,
     CourseCard,
     About
+  },
+  async asyncData ({ $axios, app: { $jsonApiStore } }) {
+
+    //     Title	ID
+    // Competitive Prog	17
+    //  Dynamic Programming	86
+    //  Interview prep c++	44
+    // Java	25
+    // FAANG	129
+    const courseIds = [17, 86, 44, 25, 129]
+
+    const {data: coursesPayload} = await $axios.get(`/courses`, {
+      params: {
+        include: 'instructors,runs',
+        exclude: 'ratings,instructors.*,feedbacks,runs.*',
+        filter: {
+          recommended: true,
+          unlisted: false,
+          id: {
+            $in: courseIds
+          }
+        },
+        sort: 'difficulty'
+      }
+    })
+
+    $jsonApiStore.reset()
+    const courses = $jsonApiStore.sync(coursesPayload)
+
+    return {
+      courses
+    }
+  },
+  data () {
+    return { courses: [] }
   }
 }
 </script>
