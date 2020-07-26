caecfb64f8e77dc3b6fed720f7d2418eac73b4af
Himank Bhalla <himank.bhalla@codingblocks.com>
2020-07-26 12:30:40 +0530

    responsive timer , add quiz title

--- online.codingblocks.com/components/Base/EligibilityTest/Question.vue
+++ online.codingblocks.com/components/Base/EligibilityTest/Question.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="br-10 p-4 border">
-    <div class="font-sm mb-5 pb-5 border-bottom">
-      Skill Assessment Test: Frontend Developement
+    <div class="font-sm mb-3 pb-3 border-bottom">
+        {{quizDescription}}
     </div>
 
     <VAsync :task="fetchQuestion" :emberStyle="true" > 
       <template v-slot="{ value: question }">
-        <div class="row no-gutters align-items-center justify-content-between mb-2">
+        <div class="row no-gutters align-items-center justify-content-between mb-2 mobile-timer">
           <div class="flex-1 pr-4">
             <div class="font-md bold">
               {{ question.description }}
@@ -57,7 +57,8 @@ export default {
     switchToNextQuestion: Function,
     userResponses: Array,
      total: Number,
-     currentIndex: Number
+     currentIndex: Number,
+     quizDescription: String
   },
   components: {
     Choice,
@@ -122,3 +123,10 @@ export default {
   }
 }
 </script>
+<style>
+@media only screen and (max-width: 768px) {
+    .mobile-timer {
+        flex-direction: column-reverse;
+    }
+}
+</style>
