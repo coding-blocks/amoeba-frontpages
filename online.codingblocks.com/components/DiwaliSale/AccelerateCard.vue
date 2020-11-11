e496aeb2cedade888037524aaf5e60fe39210b24
Hitesh Bajaj <hitesshbajaj@gmail.com>
2020-11-11 19:27:14 +0530

    coupon code removed

--- online.codingblocks.com/components/DiwaliSale/AccelerateCard.vue
+++ online.codingblocks.com/components/DiwaliSale/AccelerateCard.vue
@@ -11,14 +11,6 @@
           </div>
         </div>
         <!-- 10px of border plus 1.5rem of pl-4 -->
-        <div class="mt-5" style="padding-left: 2.125rem">
-          <div class="font-mds">Coupon Code</div>
-          <div class="d-flex mt-3">
-            <div class="dashed-border p-2 font-md bold br-10">
-              DIWALILIVE
-            </div>
-          </div>
-        </div>
       </div>
       <div class="col-6 d-flex flex-col justify-content-between align-items-center">
         <div class="percentage">
