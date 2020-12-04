ac11d7f14c4301c18c39578a902256854a472dee
Hitesh Bajaj <hitesshbajaj@gmail.com>
2020-12-05 00:12:29 +0530

    remove cricket-cup

--- online.codingblocks.com/components/TheSideBar.vue
+++ online.codingblocks.com/components/TheSideBar.vue
@@ -5,12 +5,6 @@
     </a>
     <div class="row all-center">
       <div class="col-2 col-md-12">
-        <nuxt-link to="/cricket-cup" class="nav-element py-4 px-2">
-          <img src="https://minio.codingblocks.com/public/cricket-cup-logo.png"/>
-          <div class="pt-2 d-none d-md-block font-sm card-md yellow">Cricket Cup</div>
-        </nuxt-link>
-      </div>
-      <div class="col-2 col-md-12">
         <a href="/app/dashboard" class="nav-element py-4 px-2">
           <img src="https://cb-thumbnails.s3.ap-south-1.amazonaws.com/dashboard_grey.svg"/>
           <div class="pt-2 d-none d-md-block font-sm card-md">Dashboard</div>
